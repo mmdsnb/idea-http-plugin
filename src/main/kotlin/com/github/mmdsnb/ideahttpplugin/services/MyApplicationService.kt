c97edd3292d89543469f653f0445c21d54efcbc4
GitHub Action <action@github.com>
2021-12-26 11:54:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/mmdsnb/ideahttpplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mmdsnb/ideahttpplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mmdsnb.ideahttpplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mmdsnb.ideahttpplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/mmdsnb/ideahttpplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mmdsnb/ideahttpplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mmdsnb.ideahttpplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mmdsnb.ideahttpplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
